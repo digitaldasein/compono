5b1fcb3588f7b81a4084c08a70cb1ba9a02028c5
svbaelen <senne@svbaelen.me>
2022-08-17 00:20:59 +0200

    learn more rust

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,17 @@
 // - check if file exists and ask to overwrite
 // - add templates
 
-use std::io::{self, Write};
+//use std::io::{self, Write, stdin, stdout, BufReader, prelude::*};
+use std::io::{self, Write, BufReader, prelude::*};
 use std::path::Path;
 use std::fs;
+use std::env;
 use std::process;
 use clap::{Parser, ArgEnum, Subcommand};
 use anyhow::{Context, Result};
+use git2::{Repository, Oid, Remote, PushOptions, RemoteCallbacks, Cred};
+use walkdir::{WalkDir};
+use glob::glob;
 
 /*---------------------------------------------------------------------
  * Config
@@ -52,6 +57,10 @@ const CSS_ROBOTO_FNAME:&str = "roboto_font.css";
 const CSS_DD_BASIC:&str = include_str!("./styles/dd_basic.css");
 const CSS_SHOWER_DD_BASIC:&str = include_str!("./styles/shower_dd_basic.css");
 
+const DEFAULT_SSH_KEY_PATH_STR:&str = "$HOME/.ssh/id_rsa";
+const DEFAULT_SSH_KEY_HOME:&str = ".ssh/id_rsa";
+const DEFAULT_SSH_KEY_PASSPHRASE:&str = "None";
+
 // CLI
 #[derive(Parser)]
 #[clap(author, version,  long_about = None)]
@@ -66,8 +75,8 @@ struct Cli {
 #[derive(Subcommand)]
 enum Commands {
     /// Initialise new HTML presentation
-    #[clap(visible_aliases = &["init", "create"]) ]
-    New {
+    #[clap(visible_aliases = &["init", "new"]) ]
+    Create {
         /// Use HTML template for presentation.
         /// For a custom template path, see the '--template-path option'.
         #[clap(short, long, value_parser, arg_enum,
@@ -108,6 +117,45 @@ enum Commands {
         #[clap(short, long, action)]
         no_inline_fonts: bool,
     },
+
+    /// Publish HTML presentation
+    #[clap(visible_aliases = &["pub"]) ]
+    Publish {
+        /// Path to presentation directory
+        #[clap(short, long, value_parser, default_value="./")]
+        input_dir: std::path::PathBuf,
+
+        /// Path to SSH key for gitlab/github authentication
+        #[clap(short, long, value_parser, default_value=DEFAULT_SSH_KEY_PATH_STR)]
+        ssh_key: std::path::PathBuf,
+
+        /// SSH passphrase for gitlab/github
+        #[clap(short='p', long, value_parser, default_value=DEFAULT_SSH_KEY_PASSPHRASE)]
+        ssh_passphrase: String,
+
+        /// Remote endpoint (IP address/URL). By default (git-context), it
+        /// uses the remote origin endpoint from the git repo config
+        #[clap(short='r', long, value_parser, default_value="auto")]
+        remote_endpoint: String,
+
+        /// Git* commit message when pushing to remote
+        #[clap(short, long, value_parser,
+               default_value="Publish HTML presentation")]
+        commit_msg: String,
+
+        /// Publication method (each method has its own implications,
+        /// and thus might require additional arguments)
+        #[clap(short, long, value_parser, arg_enum,
+               default_value_t = Method::Gitlab) ]
+        method: Method,
+
+        /// Determine which files to include for publishing. (Note that when
+        /// using index.html to check for files, gitignore is also checked.)
+        #[clap(short='I', long, value_parser, arg_enum,
+               default_value_t = PublishIncludeOpt::UseHtml) ]
+        include: PublishIncludeOpt,
+    }
+
 }
 
 #[derive(Clone, ArgEnum, Debug)]
@@ -132,8 +180,31 @@ enum UpdateHeaderOpt {
     JsModule
 }
 
+#[derive(Clone, ArgEnum, Debug)]
+enum PublishIncludeOpt {
+    UseHtml,
+    UseGitignore,
+    All
+}
+
+#[derive(Clone, ArgEnum, Debug)]
+enum Method {
+    Gitlab,
+    Github,
+    Scp
+}
+
+macro_rules! t {
+    ($e:expr) => {
+        match $e {
+            Ok(e) => e,
+            Err(e) => panic!("{} failed with {}", stringify!($e), e),
+        }
+    };
+}
+
 /*---------------------------------------------------------------------
- * Methods
+ * Helper functinos
  *---------------------------------------------------------------------*/
 
 //println!("{}", Path::new("/etc/hosts").exists());
@@ -193,6 +264,203 @@ fn update_header(v_html:& mut Vec<String>, filename:&str, part:UpdateHeaderOpt) 
     v_html[0] = new_header;
 
 }
+
+fn git_add(repo:&Repository, v_files: & mut Vec::<String>)
+            -> Result<(), git2::Error> {
+
+    let mut index = repo.index().expect("cannot get the Index file");
+
+    //index.add_all(v_files.iter(), IndexAddOption::DEFAULT, None)?;
+
+    for filestr in v_files.iter() {
+        //index.add_path()?;
+        let path = Path::new(filestr).strip_prefix("./").
+            expect("Failed to strip prefix from file");
+        index.add_path(path)?;
+    }
+
+    index.write()?;
+    Ok(())
+}
+
+
+fn git_commit(repo: &Repository, msg:&str) -> (Oid, Oid) {
+    let mut index = t!(repo.index());
+
+    let tree_id = t!(index.write_tree());
+    let tree = t!(repo.find_tree(tree_id));
+    let sig = t!(repo.signature());
+
+    //let head = t!(repo.head());
+
+    let head_id = match repo.refname_to_id("HEAD") {
+        Ok(oid) => oid,
+        Err(error) => panic!("Failed to get HEAD id\n
+======== HINTS ========
+1. Make sure the remote branch is added, e.g., git remote add origin <remote>
+2. Make sure to set your git upstream branch, e.g., to `main`.
+You could, for instance, performan an (initial) state + commit + push \
+to the remote origin, i.e.,:
+
+    git add <some-file>
+    git commit -m `initial`
+    git push -u origin main
+
+3. When a non-fastforwardable reference error occurs, make sure to fetch from \
+remote first (using your own fetch strategy), e.g.:
+
+    git pull origin main
+
+======================"),
+    };
+
+    //let head_id = t!(repo.refname_to_id("HEAD"));
+    let parent = t!(repo.find_commit(head_id));
+    //let msg = "Publish HTML presentation".to_string();
+    let commit = t!(repo.commit(Some("HEAD"), &sig, &sig, &msg, &tree, &[&parent]));
+
+    (commit, tree_id)
+}
+
+/*
+fn user_input(question:&str) -> String {
+
+    let mut answer=String::new();
+    print!("{}", question);
+    let _=stdout().flush();
+    stdin().read_line(&mut answer).expect("Did not enter a correct string");
+
+    // trim newlines
+    if let Some('\n')=answer.chars().next_back() {
+        answer.pop();
+    }
+    if let Some('\r')=answer.chars().next_back() {
+        answer.pop();
+    }
+
+    answer
+}
+*/
+
+fn in_gitignore(gitignore_path:&Path, entry:&Path)
+                -> Result<bool, Box<dyn std::error::Error>> {
+
+    if !gitignore_path.exists() {
+        return Ok(false)
+    }
+
+    let gitignore_file = fs::File::open(gitignore_path)?;
+    let gitignore_reader = BufReader::new(gitignore_file);
+
+    for line in gitignore_reader.lines() {
+        for git_entry in glob(&line?).expect("Failed to read glob pattern") {
+            match git_entry {
+                Ok(path) => {
+                    if let Some(path_string) = path.to_str() {
+                        if let Some(entry_str) = entry.to_str() {
+                            if entry_str.contains(path_string) {
+                                return Ok(true)
+                            }
+                        }
+                    }
+
+                },
+                Err(e) => println!("{:?}", e),
+            }
+        }
+    }
+
+    Ok(false)
+}
+
+
+fn include_files(input_dir:&std::path::PathBuf,
+                 method:&PublishIncludeOpt,
+                 v_files: & mut Vec::<String>,
+                 v_files_excl: & mut Vec::<String>)
+                 -> Result<(), Box<dyn std::error::Error>>  {
+
+    let walker = WalkDir::new(input_dir).into_iter()
+                                        .filter_map(|e| e.ok());
+
+    let html_file = Path::new(input_dir).join("index.html");
+    let html_file_str = html_file.clone()
+        .into_os_string()
+        .into_string()
+        .expect("Failed to convert html path to string");
+
+    v_files.push(html_file_str);
+
+    let gitignore_file = &Path::new(input_dir).join(".gitignore");
+
+    let html_string = fs::read_to_string(&html_file)
+            .with_context(|| format!("Failed to read file `{}`",
+                                     html_file.display()))?;
+
+    for entry in walker {
+        if entry.file_type().is_file() {
+            // check if actually used as src in index.html
+            let in_html:bool = entry.file_name()
+                .to_str()
+                .map(|s| html_string.contains(s))
+                .unwrap_or(false);
+
+            // check if part of the .git path
+            let is_git:bool = entry.path()
+                .to_str()
+                .map(|s| s.contains(".git"))
+                .unwrap_or(false);
+
+            // check if set in gitignore
+            let in_gitignore:bool = in_gitignore(gitignore_file, entry.path())?;
+
+            match method {
+                PublishIncludeOpt::All => {
+                    if !is_git {
+                        v_files.push(entry.path()
+                            .to_str()
+                            .expect("failed to convert entry to string")
+                            .to_string());
+                    }
+                }
+
+                PublishIncludeOpt::UseGitignore => {
+                    if !is_git && !in_gitignore {
+                        v_files.push(entry.path()
+                            .to_str()
+                            .expect("failed to convert entry to string")
+                            .to_string());
+                    } else if !is_git {
+                        v_files_excl.push(entry.path()
+                            .to_str()
+                            .expect("failed to convert entry to string")
+                            .to_string());
+                    }
+                }
+
+                PublishIncludeOpt::UseHtml => {
+                    if !is_git && !in_gitignore  && in_html {
+                        v_files.push(entry.path()
+                            .to_str()
+                            .expect("failed to convert entry to string")
+                            .to_string());
+                    } else if !is_git {
+                        v_files_excl.push(entry.path()
+                            .to_str()
+                            .expect("failed to convert entry to string")
+                            .to_string());
+                    }
+                }
+            };
+        }
+    }
+
+    Ok(())
+}
+
+/*---------------------------------------------------------------------
+ * Main functinos
+ *---------------------------------------------------------------------*/
 
 fn create_presentation(template:&Template,
                        template_path:&Option<std::path::PathBuf>,
@@ -376,7 +644,6 @@ from `{}`", css_path.display()))?;
                     update_header(&mut v_html_content, fname_str, UpdateHeaderOpt::Css);
                 }
             }
-
         }
 
         // scripts
@@ -444,6 +711,78 @@ from `{}`", css_path.display()))?;
     Ok(())
 }
 
+fn publish_presentation(input_dir:&std::path::PathBuf,
+                        commit_msg:&str, method:&Method,
+                        ssh_key:&std::path::PathBuf, ssh_passphrase:&str,
+                        remote_endpoint:&str, include:&PublishIncludeOpt)
+                       -> Result<(), Box<dyn std::error::Error>> {
+    let repo = match Repository::open(input_dir) {
+        Ok(repo) => repo,
+        Err(e) => panic!("Failed to open git repository: {}", e),
+    };
+
+    //println!("{}", repo.path().display());
+
+    //let username:String = user_input("username: ");
+    let mut v_files_incl = Vec::<String>::new();
+    let mut v_files_excl = Vec::<String>::new();
+
+    include_files(input_dir, include, &mut v_files_incl,  &mut v_files_excl)?;
+
+    /*let gitignore_file = input_dir.join(".gitignore");*/
+    /*let gitignore_content = if gitignore_file.exists() {*/
+        /*std::fs::read_to_string(&gitignore_file)*/
+            /*.with_context(|| format!("Failed to read .gitignore file"))?*/
+    /*} else {*/
+        /*"".to_string()*/
+    /*};*/
+
+    git_add(&repo, &mut v_files_incl)?;
+    let (commit, three_id) = git_commit(&repo, &commit_msg);
+
+    let mut cb = RemoteCallbacks::new();
+    let mut push_opts = PushOptions::new();
+
+    let head = repo.head().unwrap();
+    let refspecs: &[&str] = &[head.name().unwrap()];
+
+    // https://github.com/rust-lang/git2-rs/issues/823
+    if ssh_key.to_str().unwrap() == DEFAULT_SSH_KEY_PATH_STR {
+        cb.credentials(|_url, username_from_url, _allowed_types| {
+            Cred::ssh_key(
+                username_from_url.unwrap(),
+                None,
+                Path::new(&format!("{0}/{1}", env::var("HOME").unwrap(),
+                                   DEFAULT_SSH_KEY_HOME)),
+                Some(ssh_passphrase),
+            )
+        });
+    } else {
+        cb.credentials(|_url, username_from_url, _allowed_types| {
+            Cred::ssh_key(
+                username_from_url.unwrap(),
+                None,
+                ssh_key,
+                Some(ssh_passphrase),
+            )
+        });
+    };
+
+    push_opts.remote_callbacks(cb);
+
+    //let pushOption: Option<&mut PushOptions<'_>> = Some(pushOpts);
+    let mut remote:Remote = t!(repo.find_remote("origin"));
+
+    // push
+    println!("Pushing to remote...");
+    remote.push(refspecs, Some(&mut push_opts))
+        .with_context(|| format!("Failed to push to remote repo. \
+Make sure to provide proper SSH credentials by using options \
+`--ssh-key` and/or `--ssh-passphrase`{}", ""))?;
+    Ok(())
+}
+
+
 /*---------------------------------------------------------------------
  * Main
  *---------------------------------------------------------------------*/
@@ -455,9 +794,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // You can check for the existence of subcommands, and if found use their
     // matches just as you would the top level cmd
     match &cli.command {
-        Commands::New { template, template_path, inline,
-                        css, css_path, no_inline_fonts,
-                        output_dir, output_filename, shower } => {
+        Commands::Create { template, template_path, inline,
+                           css, css_path, no_inline_fonts,
+                           output_dir, output_filename, shower } => {
             create_presentation(&template,
                                 template_path,
                                 &css,
@@ -467,6 +806,17 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                                 shower,
                                 no_inline_fonts,
                                 inline)?;
+        }
+        Commands::Publish { input_dir, commit_msg, method,
+                            ssh_key, ssh_passphrase, remote_endpoint,
+                            include } => {
+            publish_presentation(input_dir,
+                                 &commit_msg,
+                                 &method,
+                                 &ssh_key,
+                                 ssh_passphrase,
+                                 remote_endpoint,
+                                 &include)?;
         }
     }
 
